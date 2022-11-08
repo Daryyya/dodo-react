4325c547e5ba047fe1988618c4bc4f0f3b0aec42
Malerator <112635636+Malerator@users.noreply.github.com>
2022-11-08 13:30:08 +0300

    fix

--- src/ui/Modal/index.jsx
+++ src/ui/Modal/index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import style from "./style.module.css";
-import {ingredients} from './ingredients.js'
+import { ingredients } from "./ingredients.js";
 import Buttons from "../Buttons";
 
-const Modal = ({ setIsOpen, id, data, choices, unchoices, isChoice, setId }) => {
+const Modal = ({
+  setIsOpen,
+  id,
+  data,
+  choices,
+  unchoices,
+  isChoice,
+  setId,
+}) => {
   const card = (data || []).find((el) => el.id === id);
   const ingredient = card.ingredients;
   return (
@@ -22,55 +30,55 @@ const Modal = ({ setIsOpen, id, data, choices, unchoices, isChoice, setId }) => 
           <p className={style.title}>{card.name}</p>
           <p className={style.text}>30 см, традиционное тесто, 480 г</p>
           <div className={style.checkWrap}>
-          {
-            
-            ingredient.map(({name, removable}) => (
-              removable?
-              (<label>
-                <input className={style.checkInput} type='checkbox'/>
-                <span className={style.ingredientNameRemove}>{`${name} `}</span>
-                <span className={style.check} ></span>
-              </label>) :
-              (<span className={style.ingredientName}>{`${name}, `}</span>)
-            ))
-            
-          }
+            {ingredient.map(({ name, removable }) =>
+              removable ? (
+                <label>
+                  <input className={style.checkInput} type="checkbox" />
+                  <span
+                    className={style.ingredientNameRemove}
+                  >{`${name} `}</span>
+                  <span className={style.check}></span>
+                </label>
+              ) : (
+                <span className={style.ingredientName}>{`${name}, `}</span>
+              )
+            )}
           </div>
           <div className={style.radioWrap}>
-          <label>
-          <input className={style.radioInput} type="radio" name="size" />
-          <span className={style.radio}>Маленькая</span>
-          </label>
-          <label>
-          <input className={style.radioInput} type="radio" name="size" />
-          <span className={style.radio}>Средняя</span>
-          </label>
-          <label>
-          <input className={style.radioInput} type="radio" name="size" />
-          <span className={style.radio}>Большая</span>
-          </label>
+            <label>
+              <input className={style.radioInput} type="radio" name="size" />
+              <span className={style.radio}>Маленькая</span>
+            </label>
+            <label>
+              <input className={style.radioInput} type="radio" name="size" />
+              <span className={style.radio}>Средняя</span>
+            </label>
+            <label>
+              <input className={style.radioInput} type="radio" name="size" />
+              <span className={style.radio}>Большая</span>
+            </label>
           </div>
 
           <div className={style.radioWrap}>
-          <label>
-          <input className={style.radioInput} type="radio" name="size" />
-          <span className={style.radio}>Традиционное</span>
-          </label>
-          <label>
-          <input className={style.radioInput} type="radio" name="size" />
-          <span className={style.radio}>Тонкое</span>
-          </label>
+            <label>
+              <input className={style.radioInput} type="radio" name="size" />
+              <span className={style.radio}>Традиционное</span>
+            </label>
+            <label>
+              <input className={style.radioInput} type="radio" name="size" />
+              <span className={style.radio}>Тонкое</span>
+            </label>
           </div>
 
           <h2 className={style.addToPizza}>Добавить в пиццу</h2>
           <div className={style.addToPizzaContainer}>
-          {
-            ingredients.map(el => <div className={style.card}>
-              <img src={el.image}/>
-              <h2 className={style.cardText}>{el.name}</h2>
-              <p className={style.cardPrice}>{`${el.price}₽`}</p>
-            </div>)
-          }
+            {ingredients.map((el) => (
+              <div className={style.card}>
+                <img src={el.image} />
+                <h2 className={style.cardText}>{el.name}</h2>
+                <p className={style.cardPrice}>{`${el.price}₽`}</p>
+              </div>
+            ))}
           </div>
           <Buttons
             type="orange"
